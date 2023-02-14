b48a317af89e7be6feb9669f5ba1b4661d04e60b
Mickael Salvas <mickael_salvas21@hotmail.com>
2023-02-14 18:46:23 -0500

    Added sobel

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import os
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread('./assets/car.JPG', 0)
-cv2.imshow('image', img)
-cv2.waitKey(0)
+images = os.listdir('./assets')
 
-blurImg = cv2.GaussianBlur(img, (5, 5), 0)
-ret, otsuImg = cv2.threshold(blurImg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-cv2.imshow('otsu image', otsuImg)
-cv2.waitKey(0)
+for imageName in images:
+    image = cv2.imread(f'./assets/{imageName}', 0)
+    img = cv2.GaussianBlur(image, (7, 7), 0)
 
-kernel = np.ones((2, 2), np.uint8)
-otsuImg = cv2.morphologyEx(otsuImg, cv2.MORPH_OPEN, kernel)
-otsuImg = cv2.morphologyEx(otsuImg, cv2.MORPH_CLOSE, kernel)
-contours, hierarchy = cv2.findContours(otsuImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+    gX = cv2.Sobel(img, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=5)
+    gY = cv2.Sobel(img, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=5)
+    gX = cv2.convertScaleAbs(gX)
+    gY = cv2.convertScaleAbs(gY)
+    AgXgY = cv2.addWeighted(gX, 0.5, gY, 0.5, 0)
 
-cv2.imshow('contours image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 2), plt.imshow(gX, cmap='gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 3), plt.imshow(gY, cmap='gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 4), plt.imshow(AgXgY, cmap='gray')
+    plt.title('Amplitude gX gY'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
