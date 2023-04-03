9c1cafc2fddc0a00ae9965d6259c7b75a4a41484
Max135 <maxlamenace_2000@hotmail.com>
2023-04-02 22:30:24 -0400

    Removed ending in png condition

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ import image_filters
 images = os.listdir('./assets')
 
 for imageName in images:
-    if ".png" in imageName:
-        img_bgr = cv2.imread(f'./assets/{imageName}')
-        gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-        image = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    img_bgr = cv2.imread(f'./assets/{imageName}')
+    gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
-        image_filters.filter_image(gray)
+    image_filters.filter_image(gray)
 
-        extracted_cars = ec.extract_cars(image)
-        for car in extracted_cars:
-            plt.imshow(car)
-            plt.show()
+    extracted_cars = ec.extract_cars(image)
+    for car in extracted_cars:
+        plt.imshow(car)
+        plt.show()
+
+# https://github.com/Spidy20/Car_Detection_System/blob/master/Car_detection.py
+# https://techvidvan.com/tutorials/opencv-vehicle-detection-classification-counting/
+# https://medium.com/analytics-vidhya/detecting-the-colors-of-the-vehicle-2904e8b669f8
