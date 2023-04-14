f230d6352c7642c0ae49e57a324a345930d9df45
Mickael Salvas <mickael_salvas21@hotmail.com>
2023-04-14 11:40:56 -0400

    Small refactor of tracker

--- tracker.py
+++ tracker.py
@@ -1,56 +1,49 @@
-# TechVidvan Vehicle-Tracker
-
 import math
+
 
 class EuclideanDistTracker:
     def __init__(self):
         # Store the center positions of the objects
         self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
+
+        # Keep the count of the IDs, each time a new object id detected, the count will increase by one
         self.id_count = 0
 
-
-    def update(self, objects_rect):
-        # Objects boxes and ids
-        objects_bbs_ids = []
+    def update(self, objects_rectangles):
+        # Objects bounding boxes and ids
+        objects_bb_ids = []
 
         # Get center point of new object
-        for rect in objects_rect:
-            x, y, w, h, index = rect
-            cx = (x + x + w) // 2
-            cy = (y + y + h) // 2
+        for rectangle in objects_rectangles:
+            x, y, width, height, index = rectangle
+            cx = (x + x + width) // 2
+            cy = (y + y + height) // 2
 
             # Find out if that object was detected already
             same_object_detected = False
-            for id, pt in self.center_points.items():
-                dist = math.hypot(cx - pt[0], cy - pt[1])
+            for id, point in self.center_points.items():
+                distance = math.hypot(cx - point[0], cy - point[1])
 
-                if dist < 25:
+                # can change the detection distance threshold (basic = 25)
+                if distance < 30:
                     self.center_points[id] = (cx, cy)
-                    # print(self.center_points)
-                    objects_bbs_ids.append([x, y, w, h, id, index])
+                    objects_bb_ids.append([x, y, width, height, id, index])
                     same_object_detected = True
                     break
 
             # New object is detected we assign the ID to that object
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
-                objects_bbs_ids.append([x, y, w, h, self.id_count, index])
+                objects_bb_ids.append([x, y, width, height, self.id_count, index])
                 self.id_count += 1
 
         # Clean the dictionary by center points to remove IDS not used anymore
         new_center_points = {}
-        for obj_bb_id in objects_bbs_ids:
-            _, _, _, _, object_id, index = obj_bb_id
+        for object_bb_id in objects_bb_ids:
+            _, _, _, _, object_id, index = object_bb_id
             center = self.center_points[object_id]
             new_center_points[object_id] = center
 
-        # Update dictionary with IDs not used removed
+        # Update dictionary
         self.center_points = new_center_points.copy()
-        return objects_bbs_ids
-
-
-
-def ad(a, b):
-    return a+b
+        return objects_bb_ids
